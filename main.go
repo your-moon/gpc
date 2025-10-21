47bbe4ad2f8fc139102a4dbd10f13e0298a7e035
Munkherdene <muunuu960@gmail.com>
2025-10-21 19:09:20 +0800

    refactor: remove unused files and simplify main function
    - Deleted the preloadcheck package and its associated tests, benchmarks, and documentation as part of a cleanup effort.
    - Simplified the main function in main.go by removing unnecessary imports and code, streamlining the entry point of the application.
    - This commit focuses on reducing clutter and improving maintainability of the codebase.

--- main.go
+++ main.go
@@ -1,11 +1,5 @@
 package main
 
-import (
-	"golang.org/x/tools/go/analysis/singlechecker"
-
-	preloadcheck "github.com/your-moon/gpc/pkg/preloadcheck"
-)
-
 func main() {
-	singlechecker.Main(preloadcheck.Analyzer)
+
 }
